1b1347fd5979e701654d69adcef43c3dc1ac32c6
Ronald Chanou <rchanou@gmail.com>
2015-09-10 11:38:03 -0700

    use es6 for build script

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # babel-react-opt-test
 Trying to get React 0.14 Babel optimizations to work.
 
+## Instructions
+
+If you're using Node v4.0.0+...
 ```
 npm install
 node build
 ```
+If not, include any necessary flags for the `node build` command so that Node
+can parse the ES6 syntax in the `build` script.
 
 Then open index.html in the build folder.
--- build.js
+++ build.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 
-var config = {
+const config = {
   entry: [
     path.join(__dirname, 'src', 'index')
   ],
@@ -22,7 +22,10 @@ var config = {
       {
         test: /\.jsx?$/,
         //loader: 'babel?{ "stage": 0 }',
-        loader: 'babel?{ "stage": 0, "optional": ["optimisation.react.inlineElements"] }',
+        loader: `babel?{
+          "stage": 0,
+          "optional": ["optimisation.react.inlineElements"]
+        }`,
         include: path.join(__dirname, 'src')
       }
     ]
@@ -30,14 +33,14 @@ var config = {
 };
 
 
-var compiler = webpack(config);
+const compiler = webpack(config);
 
 console.log('Starting file-watching and build process. Press Ctrl+C to exit.');
 
-compiler.watch(333, function(err, stats){
+compiler.watch(333, (err, stats) => {
   if (stats){
     if (stats.hasErrors()){
-      stats.toJson().errors.forEach(function(e){ console.error(e); });
+      stats.toJson().errors.forEach(e => { console.error(e); });
       console.log('----------------------------------------------------------');
     } else {
       console.log('Build done at ' + new Date().toString() );
